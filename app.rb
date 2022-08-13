b4271d28b2b6aab9b0d8bea74b489cef4cecd786
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-08-13 11:17:39 +0300

    commit tree 65fb54a34a70234b0239f79cd5d89ad779c00e20

---
+++ app.rb
@@ -0,0 +1,21 @@
+
+#when printing strings and add a line break at the end of the strings
+puts "Hello World!"
+# puts("Hello world")
+# puts "Hello, world"
+
+#print does not add a line break at the end of the strings
+print "Pass this test, please."
+# print "Hello, world"
+# print "Hello, world"
+
+# puts [1, 2, 3]
+
+#when printing arrays or numbers use p . puts converts them to strings
+p [1, 2, 3]
+
+
+# when printing nested arrays use pp
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
